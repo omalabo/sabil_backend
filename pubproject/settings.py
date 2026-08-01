cff5b0ebf17fde614c7a5f8950367882f79f71ca
omalabo <35241590+omalabo@users.noreply.github.com>
2026-08-02 00:54:37 +0100

    Update settings.py

--- pubproject/settings.py
+++ pubproject/settings.py
@@ -29,17 +29,8 @@ DEBUG = False
 
 # Autoriser les domaines de production (séparés par des virgules dans l'env)
 
-#allowed_hosts = os.environ.get('DJANGO_ALLOWED_HOSTS', 'localhost,127.0.0.1')
-#ALLOWED_HOSTS = allowed_hosts.split(',') if allowed_hosts else ['localhost', '127.0.0.1'],
-
-ALLOWED_HOSTS = [
-    '57.131.134.241',           # IP publique de ton serveur OVH
-    'localhost',
-    '127.0.0.1',
-    # Si tu as un nom de domaine, ajoute-le aussi :
-    # 'tondomaine.com',
-    # 'www.tondomaine.com',
-]
+allowed_hosts = os.environ.get('DJANGO_ALLOWED_HOSTS', 'localhost,127.0.0.1,57.131.134.241')
+ALLOWED_HOSTS = allowed_hosts.split(',') if allowed_hosts else ['localhost', '127.0.0.1'],
 
 
 # Application definition
